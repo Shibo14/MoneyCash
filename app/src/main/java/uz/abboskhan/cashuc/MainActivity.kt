5760e5fe9f00acbcfdf5a11378e253948af205a3
elbek005 <elbeksobitaliyev663@gmail.com>
2023-11-02 17:45:10 +0500

    home ui add

--- app/src/main/java/uz/abboskhan/cashuc/MainActivity.kt
+++ app/src/main/java/uz/abboskhan/cashuc/MainActivity.kt
@@ -2,12 +2,52 @@ package uz.abboskhan.cashuc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import uz.abboskhan.cashuc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        binding.menuBottomNaw.setOnItemSelectedListener { i ->
+            when (i.itemId) {
+                R.id.home -> {
+                    menuButton(HomeFragment())
+                    true
+                }
+
+                R.id.pay -> {
+                    menuButton(PayFragment())
+                    true
+                }
+
+                R.id.history -> {
+                    menuButton(HistoryFragment())
+                    true
+                }
+
+                R.id.person -> {
+                    menuButton(PersonFragment())
+                    true
+                }
+
+                else -> false
+
+            }
+
+        }
+        menuButton(HomeFragment())
+
+    }
+
+    private fun menuButton(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction().replace(R.id.frm_layout, fragment).commit()
 
     }
 }
